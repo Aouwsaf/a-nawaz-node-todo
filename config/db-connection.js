ff347a966ca66cf4ad330e3ae60c36d2508abc6d
aouwsaf@gmail.com <aouwsaf@gmail.com>
2025-05-03 16:49:08 +0530

    Updating Package.JSON

--- config/db-connection.js
+++ config/db-connection.js
@@ -1,16 +1,33 @@
-const mongoDbClientConnect = require('mongodb').MongoClient
+const { MongoClient } = require('mongodb');
 
-const connectMongo = mongoDbClientConnect.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false',
-  { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
-    if (err) {
-      console.log("Database Not Connected!", err);
-      return;
-    }
-    dbObject = db.db("CronJReactNode")
-    console.log("Database Connected!");
-  })
-function GetCollectionObj(collName) {
-  return dbObject.collection(collName)
+const mongoUri = "mongodb://127.0.0.1:27017";
+const dbName = "MERNTodoApp";
+
+let dbInstance = null;
+
+async function connectMongo() {
+  if (dbInstance) return dbInstance;
+
+  try {
+    const client = await MongoClient.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    dbInstance = client.db(dbName);
+    console.log("✅ MongoDB connected to", dbName);
+    return dbInstance;
+  } catch (err) {
+    console.error("❌ MongoDB connection failed:", err);
+    throw err;
+  }
 }
 
-module.exports = { mongoDbClientConnect, GetCollectionObj }
+function GetCollectionObj(collName) {
+	if (!dbInstance) {
+		throw new Error("MongoDB not connected. Call connectMongo() first.");
+	}
+	return dbInstance.collection(collName);
+}
+
+module.exports = { connectMongo, GetCollectionObj };
--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const express = require('express')
-const app = express()
-const cookieParser = require('cookie-parser')
-const RoutesForAPI = require('./routes')
-const mongoEstdCon = require("./config/db-connection").mongoDbClientConnect
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const RoutesForAPI = require("./routes");
+const { connectMongo } = require('./config/db-connection');
 
+app.use(express.urlencoded({ extended: true })); // this will decreapt the sent data through URL
+app.use(express.json()); // this will enable the body pasrser for JSON type
+app.use((req, res, next) => {
+	// console.log('Recieved', req)
+	// res.setHeader("Access-Control-Allow-Origin", "same-origin");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	// res.set('Access-Control-Allow-Origin', 'same-origin');
+	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	res.setHeader("Content-Type", "application/json");
+	res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE, OPTIONS");
+	next();
+});
 
+app.use("/api", RoutesForAPI);
 
-app.use(express.urlencoded({extended: true})); // this will decreapt the sent data through URL
-app.use(express.json()) // this will enable the body pasrser for JSON type
-app.use((req, res, next)=>{
-  // console.log('Recieved', req)
-  // res.setHeader("Access-Control-Allow-Origin", "same-origin");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.set('Access-Control-Allow-Origin', 'same-origin');
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  )
-  res.setHeader("Content-Type", "application/json")
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
-})
+connectMongo().then(() => {
+  app.use('/api', RoutesForAPI);
 
-app.use('/api', RoutesForAPI)
-
-app.listen(8000, ()=>{
-  console.log("We are running at 8000")
-})
+  app.listen(8000, () => {
+    console.log("🚀 Server is running at http://localhost:8000");
+  });
+}).catch((err) => {
+  console.error("❌ Server startup failed due to DB error:", err);
+});
